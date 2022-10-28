486e393e3cb8f46e6935f4659cda159b82bb433e
Roosevelt <roosevelt_stenson@yahoo.com>
2022-10-27 20:20:54 -0500

    brian-armweak
    + grabbing access_token from URL hash
    + tried to install react-spotify-web-playback, getting errors that make no sense

---
+++ web/src/Player.tsx
@@ -0,0 +1,11 @@
+import SpotifyPlayer from 'react-spotify-web-playback';
+
+function Player() {
+    return (
+        <div>
+            <SpotifyPlayer />
+        </div>
+    );
+}
+
+export default Player;
