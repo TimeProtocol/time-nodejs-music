244c5c2b640dcf922e47338feff47eec9aef0f4d
Tawnee <allprt89@gmail.com>
2022-10-31 11:24:22 -0600

    enable socketio in App. update packages

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
-// import io from "socket.io-client";
+import io from "socket.io-client";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
-// const socket = io(`${process.env.REACT_APP_TIME_ENDPOINT}`);
+const socket = io(`${process.env.REACT_APP_TIME_ENDPOINT}`);
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const code = new URLSearchParams(window.location.search).get("code");
@@ -12,38 +12,39 @@ const code = new URLSearchParams(window.location.search).get("code");
 function App() {
   const { address, isConnected } = useAccount();
   const [clientID, setClientID] = useState("");
+  console.log("clientID: ", clientID);
   // replace with what gets returned from loging into spotify
   const [isSpotifyLoggedIn, setIsSpotifyLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   socket.on("connect", () => {
-  //     console.log("connected");
-  //   });
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
 
-  //   socket.on("disconnect", () => {
-  //     console.log("disconnected");
-  //   });
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
 
-  //   socket.on("login", (data) => {
-  //     console.log(data);
-  //     setClientID(data.clientID);
-  //   });
+    socket.on("login", (data) => {
+      console.log(data);
+      setClientID(data.clientID);
+    });
 
-  //   return () => {
-  //     socket.off("connect");
-  //     socket.off("disconnect");
-  //     socket.off("login");
-  //   };
-  // }, []);
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("login");
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   if (isConnected && address) {
-  //     socket.emit("login", address);
-  //   } else {
-  //     socket.emit("logout");
-  //     setClientID("");
-  //   }
-  // }, [isConnected, address]);
+  useEffect(() => {
+    if (isConnected && address) {
+      socket.emit("login", address);
+    } else {
+      socket.emit("logout");
+      setClientID("");
+    }
+  }, [isConnected, address]);
 
   function loginToSpotify() {
     var spotifyApi = new SpotifyWebApi();
